24ceaba3426bb85aaa3a9e0fca99ed0c9c5cdde9
Casey Foster <c@sey.me>
2015-08-13 10:53:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var _ = require('underscore');
+var chalk = require('chalk');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+
+var DEFAULT_CONFIG = {
+  name: os.hostname(),
+  colors: true,
+  metrics: true
+};
+
+var config = DEFAULT_CONFIG;
+var maxIndex = Infinity;
+
+var LEVELS = ['error', 'info', 'debug'];
+
+var COLORS = {
+  error: 'red',
+  debug: 'yellow'
+};
+
+var HANDLE_ERROR = function (er) { if (er) throw er; };
+
+var write = function (type, str) {
+  str += '\n';
+  if (!config.dir) return process.stdout.write(str);
+  fs.appendFile(path.resolve(config.dir, type + '.log'), str, HANDLE_ERROR);
+};
+
+var log = function (level, index, msg) {
+  if (index > maxIndex) return;
+  var iso = (new Date()).toISOString();
+  var name = ' [' + config.name + '] ';
+  msg = iso + name + chalk.bold(level.toUpperCase()) + ' ' + msg;
+  var color = COLORS[level];
+  write(level, color ? chalk[color](msg) : msg);
+};
+
+_.each(LEVELS, function (level, index) {
+  exports[level] = _.partial(log, level, index);
+});
+
+var metric = function (type, name, metric) {
+  if (!config.metrics) return;
+  write('metrics', JSON.stringify({
+    '@timestamp': (new Date()).toISOString(),
+    app_name: config.name,
+    tags: [type],
+    service: name,
+    metric: metric
+  }));
+};
+
+exports.mark = _.partial(metric, 'mark', _, 1);
+
+exports.gauge = _.partial(metric, 'gauge');
+
+exports.duration = _.partial(metric, 'time');
+
+exports.time = function (cb) {
+  var start = Date.now();
+  cb(function (name) { metric('time', name, Date.now() - start); });
+};
+
+exports.config = function (_config) {
+  config = _.extend({}, DEFAULT_CONFIG, _config);
+  chalk.enabled = !config.dir && config.colors;
+  maxIndex = _.indexOf(LEVELS, config.level);
+  if (maxIndex === -1) maxIndex = Infinity;
+};
+
+exports.config(config);
---
+++ test.js
@@ -0,0 +1,28 @@
+var log = require('./');
+
+log.config({name: 'test'});
+log.error('type=error name=test color=red');
+log.info('type=info name=test color=default');
+log.debug('type=debug name=test color=yellow');
+log.mark('mark_metric');
+log.gauge('gauge_metric', 10);
+log.duration('duration_metric', 100);
+log.time(function (done) { done('time_metric'); });
+
+log.config({name: 'test2', level: 'error', colors: false, metrics: false});
+log.error('type=error name=test2 color=default');
+log.info('SHOULD NOT SHOW');
+log.debug('SHOULD NOT SHOW');
+log.mark('SHOULD NOT SHOW');
+log.gauge('SHOULD NOT SHOW', 10);
+log.duration('SHOULD NOT SHOW', 100);
+log.time(function (done) { done('SHOULD NOT SHOW'); });
+
+log.config({name: 'test3', dir: 'log'});
+log.error('type=error name=test color=red');
+log.info('type=info name=test color=default');
+log.debug('type=debug name=test color=yellow');
+log.mark('mark_metric');
+log.gauge('gauge_metric', 10);
+log.duration('duration_metric', 100);
+log.time(function (done) { done('time_metric'); });
